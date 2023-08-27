edc6a575ef013e3c1bd0107f5ca51b754fcdfce1
Antonette Caldwell <acald.nebula@gmail.com>
2023-08-26 21:42:03 -0500

    reconfigured core library into components and web-components

---
+++ packages/fenixui-components/src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+export type ButtonProps = {
+    children: ReactNode
+    onClick: () => void
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => {
+    return <button onClick={onClick}>{children}</button>
+}
+
+export default Button
